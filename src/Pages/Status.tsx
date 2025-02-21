f0eba4b2d28d8f33a69255218e33bdc829b94911
Lucas Söderlund <superlucke123@gmail.com>
2025-02-21 15:22:00 +0100

    update status with API

--- src/Pages/Status.tsx
+++ src/Pages/Status.tsx
@@ -1,12 +1,32 @@
 import React, { useState, useEffect } from "react";
+import { createOrder, OrderItem, OrderResponse } from "../Services/api";
+
 
 import '../Styles/Pages/status.scss'
-
 
 const Status: React.FC = () => {
     const [orderId, setOrderId] = useState<string | null>(null)
     const [eta, setEta] = useState<number | null>(null)
 
+    const handleOrder = async () => {
+        try {
+        const orderItems: OrderItem[] = [
+            {name: 'Latte', price: 1},
+            {name: 'Espresso', price: 2}
+        ]
+
+        const orderResponse: OrderResponse = await createOrder(orderItems)
+
+        setOrderId(orderResponse.orderNr)
+        setEta(orderResponse.eta)
+    } catch (error) {
+        console.log('error', error)
+    }
+}
+
+    useEffect(() => {
+        handleOrder()
+    }, [])
 
     return (
         <div>
