c340d060ae1980f977220558ae65c7580aba41c5
veleii <vendelanorinder@hotmail.se>
2025-02-24 11:58:30 +0100

    v

--- src/Components/MenuItem.tsx
+++ src/Components/MenuItem.tsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from 'react';
 import AddImg from '../assets/add.png';
+import {getMenu, MenuResponse, MenuItem} from '../Services/api';
+
 
 export default function MenuItems (){
+    const [menuItems, setMenuItems] = useState<MenuItem[]>([])
+    useEffect( () => {
+        const fetchMenu = async () => {
+            const menu = await getMenu ()
+            console.log(menu)
+            setMenuItems (menu)
+        } 
+        fetchMenu()
+    }, []) 
     return (
     <div className="MenuItems" >
+
         <img src={AddImg} alt="Add symbol" className="MenuItemsImg"/>
-
-            <div className='CoffeeInfo'>
-            <h2>Kaffesort</h2>
-            <p>Info om kaffet</p>
+            {menuItems.map((item) => (
+               <div key={item.id} className="CoffeeInfo">
+               <h3>{item.title}</h3>
+               <p>{item.desc}</p>
+               <p className="Sum">{item.price} kr</p>
             </div>
-
-        <p className='Sum'>50 kr</p>
+            ))}
     </div>
-    );
+);
 }
+
