17ec18487aac0e23cb07ee621bf7dc3ecf0cf9eb
veleii <vendelanorinder@hotmail.se>
2025-02-24 13:23:30 +0100

    v

--- src/Components/MenuItem.tsx
+++ src/Components/MenuItem.tsx
@@ -1,30 +1,28 @@
 import { useEffect, useState } from 'react';
 import AddImg from '../assets/add.png';
-import {getMenu, MenuResponse, MenuItem} from '../Services/api';
+import { getMenu, MenuResponse, MenuItem } from '../Services/api';
 
+export default function MenuItems() {
+  const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
 
-export default function MenuItems (){
-    const [menuItems, setMenuItems] = useState<MenuItem[]>([])
-    useEffect( () => {
-        const fetchMenu = async () => {
-            const menu = await getMenu ()
-            console.log(menu)
-            setMenuItems (menu)
-        } 
-        fetchMenu()
-    }, []) 
-    return (
+  useEffect(() => {
+    const fetchMenu = async () => {
+      const menu = await getMenu();
+      console.log(menu);
+      setMenuItems(menu);
+    };
+    fetchMenu();
+  }, []);
+  return (
     <div className="MenuItems" >
-
-        <img src={AddImg} alt="Add symbol" className="MenuItemsImg"/>
-            {menuItems.map((item) => (
-               <div key={item.id} className="CoffeeInfo">
-               <h3>{item.title}</h3>
-               <p>{item.desc}</p>
-               <p className="Sum">{item.price} kr</p>
-            </div>
-            ))}
+        {menuItems.map((item) => (
+           <div key={item.id} className="CoffeeInfo">
+            <img src={AddImg} alt="Add symbol" className="MenuItemsImg"/>
+           <h3>{item.title}</h3>
+           <p>{item.desc}</p>
+           <p className="Sum">{item.price} kr</p>
+        </div>
+      ))}
     </div>
-);
+  );
 }
-
