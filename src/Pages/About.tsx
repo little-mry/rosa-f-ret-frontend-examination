a7c02c69a9ac7950d58b78a06550b9945cc9d371
alicelarsson95 <alicelarsson95@hotmail.com>
2025-02-24 10:03:38 +0100

    lagt till abouttext

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -3,6 +3,7 @@ import headerButton from "../assets/navicon (2).png"
 import aboutProfile from "../assets/image 1.jpg"
 import aboutFooter from "../assets/header.png"
 import "../Styles/Pages/about.scss"
+import AboutText from "../Components/AboutText"
 
 const About = () => {
   return (
@@ -12,28 +13,7 @@ const About = () => {
         <img src={headerButton} alt="Hamburgarmeny header" className="about-header-button" />
       </header>
 
-      <div className="about-content">
-        <h1>Vårt kaffe</h1>
-        <h2>
-          Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio, grounds dripper, crema, strong
-          whipped, variety extra iced id lungo half and half mazagran. Pumpkin spice.
-        </h2>
-        <p>
-          Que dark fair trade, spoon decaffeinated, barista wings whipped, as rich aftertaste, con panna milk
-          black, arabica white rich beans single shot extra affogato. So affogato macchiato sit extraction
-          instant grinder seasonal organic, turkish single shot, single origin, and robusta strong to go so
-          dripper. Viennese froth, grounds caramelization skinny aromatic cup kopi-luwak, fair trade flavour,
-          frappuccino medium, café au lait flavour cultivar ut bar instant kopi-luwak.
-        </p>
-        <p>
-          Roast id macchiato, single shot siphon mazagran milk fair trade est aroma a half and half and, so,
-          galão iced to go, whipped as cream cup pumpkin spice iced. At extra, rich grinder, brewed to go,
-          steamed half and half at, that, percolator macchiato trifecta and body as arabica dripper. In galão
-          black java milk sit trifecta, robusta, acerbic café au lait instant shop latte. Seasonal bar shop
-          filter aroma id, crema, affogato viennese cultivar aftertaste, seasonal, percolator cream black,
-          galão flavour, milk aromatic turkish skinny crema.
-        </p>
-      </div>
+      <AboutText />
 
       <div className="about-profile">
         <img src={aboutProfile} alt="Eva Cortado" className="about-profile-img" />
@@ -45,7 +25,7 @@ const About = () => {
         <img src={aboutFooter} alt="Footer med blad" className="about-footer-img" />
       </footer>
     </div>
-  );
-};
+  )
+}
 
 export default About
