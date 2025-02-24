ec8d47f09696014bdd4f78428e98a503fe247ea3
alicelarsson95 <alicelarsson95@hotmail.com>
2025-02-24 11:47:22 +0100

    ändringar

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,17 +1,20 @@
-import headerImage from "../assets/header (2).png"
-import headerButton from "../assets/navicon (2).png"
+/* import headerImage from "../assets/header (2).png"
+import headerButton from "../assets/navicon (2).png" */
 import aboutProfile from "../assets/image 1.jpg"
 import aboutFooter from "../assets/header.png"
 import "../Styles/Pages/about.scss"
 import AboutText from "../Components/AboutText"
+import Header from "../Components/Header"
 
 const About = () => {
   return (
     <div className="about-container">
-      <header className="header">
+     {/*  <header className="header">
         <img src={headerImage} alt="header med blad" className="about-header-img" />
         <img src={headerButton} alt="Hamburgarmeny header" className="about-header-button" />
-      </header>
+      </header> */}
+
+      <Header />
 
       <AboutText />
 
