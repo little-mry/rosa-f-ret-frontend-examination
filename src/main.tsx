a22eb0f46a5af1406d9398e54811bac0cca349d1
Maria Kjellholm <maria.kjellholm@gmail.com>
2025-02-24 10:08:22 +0100

    fixed megring clonflicts

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './Styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
