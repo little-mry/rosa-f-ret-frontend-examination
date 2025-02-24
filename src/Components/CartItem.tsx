b74928bb7e3d7e039c2d7b9e4a8a7b8a00640c2b
Maria Kjellholm <maria.kjellholm@gmail.com>
2025-02-24 13:42:07 +0100

    second commit cart

--- src/Components/CartItem.tsx
+++ src/Components/CartItem.tsx
@@ -1,9 +1,12 @@
 import '../Styles/Components/Cart.scss'
+import addIcom from '../assets/'
 
 const CartItem = () => {
   return (
     <article className='cart__item'>
         <p className='item__title'>Bryggkaffe</p>
+        <div className='cart__dotted-line'></div>
+            <img src="" alt="" />
         <p className='item__price'>49 kr</p>
     </article>
   )
