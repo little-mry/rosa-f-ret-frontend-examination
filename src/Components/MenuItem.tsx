9c2eba572231fb98b69f8873cd118e17c475d59e
ludwig <ludwigstridh@gmail.com>
2025-02-24 13:49:56 +0100

    doing vendelas work

--- src/Components/MenuItem.tsx
+++ src/Components/MenuItem.tsx
@@ -1,28 +1,48 @@
-import { useEffect, useState } from 'react';
-import AddImg from '../assets/add.png';
-import { getMenu, MenuResponse, MenuItem } from '../Services/api';
+import { useEffect, useState } from "react";
+import AddImg from "../assets/add.png";
+import { getMenu, MenuResponse, MenuItem } from "../Services/api";
 
 export default function MenuItems() {
-  const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+	const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
 
-  useEffect(() => {
-    const fetchMenu = async () => {
-      const menu = await getMenu();
-      console.log(menu);
-      setMenuItems(menu);
-    };
-    fetchMenu();
-  }, []);
-  return (
-    <div className="MenuItems" >
-        {menuItems.map((item) => (
-           <div key={item.id} className="CoffeeInfo">
-            <img src={AddImg} alt="Add symbol" className="MenuItemsImg"/>
-           <h3>{item.title}</h3>
-           <p>{item.desc}</p>
-           <p className="Sum">{item.price} kr</p>
+	useEffect(() => {
+		const fetchMenu = async () => {
+			const menu = await getMenu();
+			console.log(menu);
+			setMenuItems(menu);
+		};
+		fetchMenu();
+	}, []);
+	return (
+		<>
+			<div className="MenuItems">
+				{menuItems.map((item) => (
+					// <div
+					// 	key={item.id}
+					// 	className="CoffeeInfo">
+					// 	<img
+					// 		src={AddImg}
+					// 		alt="Add symbol"
+					// 		className="MenuItemsImg"
+					// 	/>
+					// 	<h3>{item.title}</h3>
+					// 	<p>{item.desc}</p>
+					// 	<p className="Sum">{item.price} kr</p>
+					// </div>
+          <div className="cont">
+          <div className="left"><img
+                src={AddImg}
+                alt="Add symbol"
+                className="btn-size"
+              /></div>
+          <div className="right">
+            <div className="top">{item.title}..........{item.price}kr</div>
+            <div className="bottom">{item.desc}</div>
+          </div>
         </div>
-      ))}
-    </div>
-  );
+				))}
+			</div>
+		
+		</>
+	);
 }
