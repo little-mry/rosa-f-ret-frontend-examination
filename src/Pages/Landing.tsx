02f47e43b19e25417db6372aab12b3977682f038
Lucas Söderlund <superlucke123@gmail.com>
2025-02-24 15:28:16 +0100

    landing page updated

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -8,7 +8,7 @@ const Landing: React.FC = () => {
         <div className="wrapper">
             <img src="/src/assets/header (2).svg" alt=""  className="img-left"/>
             <img src="/src/assets/header.svg" alt="" className="img-right" />
-            <Link to='meny'>
+            <Link to='menu'>
             <img src="/src/assets/Group 7.svg" alt="" className="img-center" />
             </Link>
             <h1 className="header">AIR BEAN</h1>
