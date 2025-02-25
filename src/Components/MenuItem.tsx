afe80c070f9d2627376fc2a9b8d73d1d54314292
veleii <vendelanorinder@hotmail.se>
2025-02-25 12:35:06 +0100

    v

--- src/Components/MenuItem.tsx
+++ src/Components/MenuItem.tsx
@@ -3,49 +3,55 @@ import AddImg from "../assets/add.png";
 import { getMenu, MenuItem } from "../Services/api";
 
 export default function MenuItems() {
-	const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+  const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
 
-	useEffect(() => {
-		const fetchMenu = async () => {
-			const menu = await getMenu();
-			console.log(menu);
-			setMenuItems(menu);
-		};
-		fetchMenu();
-	}, []);
-	return (
-		<>
-			<div className="MenuItems">
-				{menuItems.map((item) => (
-					// <div
-					// 	key={item.id}
-					// 	className="CoffeeInfo">
-					// 	<img
-					// 		src={AddImg}
-					// 		alt="Add symbol"
-					// 		className="MenuItemsImg"
-					// 	/>
-					// 	<h3>{item.title}</h3>
-					// 	<p>{item.desc}</p>
-					// 	<p className="Sum">{item.price} kr</p>
-					// </div>
-          <section className="cont">
-          <figure className="img"><img
-                src={AddImg}
-                alt="Add symbol"
-                className="btn-size"
-              /></figure>
+  useEffect(() => {
+    const fetchMenu = async () => {
+      const menu = await getMenu();
+      setMenuItems(menu);
+    };
+    fetchMenu();
+  }, []);
+
+ 
+  const campaignOffer = {
+    id: "campaign", 
+    title: "Kampanjerbjudande",
+    desc: "Bryggkaffe + bakelse",
+    price: 49,
+  };
+
+  return (
+    <>
+      <div className="MenuItems">
+        <section className="cont" key={campaignOffer.id}>
+          <figure className="img">
+            <img src={AddImg} alt="Add symbol" className="btn-size" />
+          </figure>
           <section className="info">
-            <h3 className="top">{item.title}......</h3>
-            <p className="desc">{item.desc}</p>
+            <h3 className="top">{campaignOffer.title}....</h3>
+            <p className="desc">{campaignOffer.desc}</p>
           </section>
-		  <section className="sum">
-			<h4>{item.price}kr</h4>
-			</section>
+          <section className="sum">
+            <h4>{campaignOffer.price}kr</h4>
+          </section>
         </section>
-				))}
-			</div>
-		
-		</>
-	);
+
+        {menuItems.map((item) => (
+          <section className="cont" key={item.id}>
+            <figure className="img">
+              <img src={AddImg} alt="Add symbol" className="btn-size" />
+            </figure>
+            <section className="info">
+              <h3 className="top">{item.title}....</h3>
+              <p className="desc">{item.desc}</p>
+            </section>
+            <section className="sum">
+              <h4>{item.price}kr</h4>
+            </section>
+          </section>
+        ))}
+      </div>
+    </>
+  );
 }
