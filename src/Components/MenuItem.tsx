e5a612feadf3ab1b8474ba69975c7e3046905f73
veleii <vendelanorinder@hotmail.se>
2025-02-26 09:50:19 +0100

    v

--- src/Components/MenuItem.tsx
+++ src/Components/MenuItem.tsx
@@ -4,6 +4,7 @@ import { getMenu, MenuItem } from "../Services/api";
 
 export default function MenuItems() {
   const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+  const [campaign, setCampaign] = useState(false);
 
   useEffect(() => {
     const fetchMenu = async () => {
@@ -11,6 +12,11 @@ export default function MenuItems() {
       setMenuItems(menu);
     };
     fetchMenu();
+
+    const currentMonth = new Date().getMonth();
+    if (currentMonth === 1) {
+      setCampaign(true);
+    }
   }, []);
 
   const campaignOffer = {
@@ -21,39 +27,34 @@ export default function MenuItems() {
   };
 
   return (
-    <>
-      <div className="MenuItems">
-
-        <section className="cont" key={campaignOffer.id}>
+    <div className="MenuItems">
+      {campaign && (
+        <section className="cont_camp" key={campaignOffer.id}>
           <figure className="img">
             <img src={AddImg} alt="Add symbol" className="btn-size" />
           </figure>
-         
-            <h3 className="top">{campaignOffer.title}</h3>
-			<div className="menu_dotted_line"></div>
-            <p className="desc">{campaignOffer.desc}</p>
-          
+          <h3 className="top">{campaignOffer.title}</h3>
+          <div className="menu_dotted_line_camp"></div>
+          <p className="desc">{campaignOffer.desc}</p>
           <section className="sum">
             <h4>{campaignOffer.price}kr</h4>
           </section>
         </section>
+      )}
 
-        {menuItems.map((item) => (
-          <section className="cont" key={item.id}>
-            <figure className="img">
-              <img src={AddImg} alt="Add symbol" className="btn-size" />
-            </figure>
-           
-              <h3 className="top">{item.title}</h3>
-			  <div className="menu_dotted_line"></div>
-              <p className="desc">{item.desc}</p>
-            
-            <section className="sum">
-              <h4>{item.price}kr</h4>
-            </section>
+      {menuItems.map((item) => (
+        <section className="cont" key={item.id}>
+          <figure className="img">
+            <img src={AddImg} alt="Add symbol" className="btn-size" />
+          </figure>
+          <h3 className="top">{item.title}</h3>
+          <div className="menu_dotted_line"></div>
+          <p className="desc">{item.desc}</p>
+          <section className="sum">
+            <h4>{item.price}kr</h4>
           </section>
-        ))}
-      </div>
-    </>
+        </section>
+      ))}
+    </div>
   );
 }
