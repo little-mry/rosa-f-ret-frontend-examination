f201d15b16c4fd742b84c9f923ca095a04d7bcfa
alicelarsson95 <alicelarsson95@hotmail.com>
2025-02-26 10:01:54 +0100

    fix nav

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,40 +1,59 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NavIcon from "../assets/navicon.png";
-import "../Styles/Components/nav.scss"; 
+import ExitIcon from "../assets/exit-icon.png";
+import "../Styles/Components/nav.scss";
 
 const Nav = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <>
-            <img
-                src={NavIcon}
-                alt="Navigation icon"
-                className={`nav__icon ${isMenuOpen ? 'nav__icon--open' : ''}`}
-                onClick={toggleMenu}
-            />
+  return (
+    <>
+      <img
+        src= {isMenuOpen ? ExitIcon : NavIcon}
+        alt="Navigation icon"
+        className={`nav__icon ${isMenuOpen ? "nav__icon--open" : ""}`}
+        onClick={toggleMenu}
+      />
 
-            {isMenuOpen && (
-                <div className="nav__menu">
-                    <div className="nav__menu-content">
-                        <nav>
-                            <ul>
-                                <li onClick={() => { navigate('/menu'); toggleMenu(); }}>Meny</li>
-                                <li onClick={() => { navigate('/about'); toggleMenu(); }}>Vårt kaffe</li>
-                                <li onClick={() => { navigate('/status'); toggleMenu(); }}>Order Status</li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            )}
-        </>
-    );
+      {isMenuOpen && (
+        <div className="nav__menu">
+          <div className="nav__menu-content">
+            <nav>
+              <ul>
+                <li
+                  onClick={() => {
+                    navigate("/menu");
+                    toggleMenu();
+                  }}>
+                  Meny
+                </li>
+                <li
+                  onClick={() => {
+                    navigate("/about");
+                    toggleMenu();
+                  }}>
+                  Vårt kaffe
+                </li>
+                <li
+                  onClick={() => {
+                    navigate("/status");
+                    toggleMenu();
+                  }}>
+                  Orderstatus
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Nav;
